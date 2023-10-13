d1c6b2c3935e97d30d1e24f286c675b6df5e4d97
visteen <visteen192@gmail.com>
2023-10-13 09:45:22 +0000

    font awesome added

--- realtimeChat/App.jsx
+++ realtimeChat/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './AppNavigator';
 import { StatusBar } from 'react-native';
-
+import './src/fontAwosome/FontAwosome'
 function App() {
   return (
     <NavigationContainer>
--- realtimeChat/src/screens/home/Index.jsx
+++ realtimeChat/src/screens/home/Index.jsx
@@ -1,19 +1,32 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
 import Request from '../request/Index';
 import Profile from '../profile/Index';
 import Friends from '../friends/Index';
+
 const HomeScreen = () => {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator
-    initialRouteName='Request'
-    activeColor='EB6A58'
-    inactiveColor='#3e2465'
-    headerShown={false}
-    barStyle={{paddingBottom: 48}}>
+    screenOptions={({route, navigation}) => ({
+      tabBarIcon:({focused, color, size}) =>{
+        const icons ={
+          Request:'bell',
+          Friends:'inbox',
+          Profile:'user'
+        }
+        const icon = icons[route.name]
+        return(
+          <FontAwesomeIcon icon={icon} size={28} color={color}/>
+        )
+      },
+      tabBarActiveTintColor:'#202020'
+    })}
+    >
       <Tab.Screen name="Request" component={Request} options={{}} />
       <Tab.Screen name="Friends" component={Friends} options={{}} />
         <Tab.Screen name="Profile" component={Profile} options={{}} />
