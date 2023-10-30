7b18c6d4ba8451c39c48214465f567b2fdd5c531
visteen <visteen192@gmail.com>
2023-10-30 21:34:50 +0000

    getting user from the store and display user info at profile paged added

--- realtimeChat/src/screens/profile/Index.jsx
+++ realtimeChat/src/screens/profile/Index.jsx
@@ -1,22 +1,43 @@
-import {SafeAreaView, StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
-import React from 'react'
-import styles from './profile.style'
-import ProfileLogout from './ProfileLogout'
-import { useDispatch ,useSelector} from 'react-redux';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import styles from './profile.style';
+import ProfileLogout from './ProfileLogout';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+  // Use useSelector to get user information from the Redux store
+  const userLogin = useSelector((state) => state.user_login);
+  const { userInfo } = userLogin;
 
-  
-  return (
-    <View style={styles.view}>
-      <Image source={require('../../assets/666201.png')}
-          style={{ width:180, height:180, borderRadius: 90, backgroundColor:'#e0e0e0', marginBottom:20}}/>
-      <Text style={styles.name}>Louis Seyram</Text>
-      <Text style={styles.username}>@Louis</Text>
-      <ProfileLogout/>
-    </View>
-  )
-}
+  // Check if userInfo is available
+  if (userInfo) {
+    const { username, first_name, last_name } = userInfo.user;
 
-export default Profile
+    return (
+      <View style={styles.view}>
+        <Image
+          source={require('../../assets/666201.png')}
+          style={{
+            width: 180,
+            height: 180,
+            borderRadius: 90,
+            backgroundColor: '#e0e0e0',
+            marginBottom: 20,
+          }}
+        />
+        <Text style={styles.name}>{`${first_name} ${last_name}`}</Text>
+        <Text style={styles.username}>{`@${username}`}</Text>
+        <ProfileLogout />
+      </View>
+    );
+  } else {
+    // Handle the case when userInfo is not available (user not logged in)
+    return (
+      <View style={styles.view}>
+        <Text style={styles.name}>User not logged in</Text>
+      </View>
+    );
+  }
+};
 
+export default Profile;
